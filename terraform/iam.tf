2e812a124233fe6fef72f783a35cc24e3bfae85b
Dhruvnotfound <dhruvgupta3107@gmail.com>
2024-07-30 23:12:21 +0530

    added iam user

---
+++ terraform/iam.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_user" "dynamodb_user" {
+  name          = "passgobbler-tester"
+  force_destroy = true
+}
+
+resource "aws_iam_access_key" "dynamodb_access_key" {
+  user       = aws_iam_user.dynamodb_user.name
+  depends_on = [aws_iam_user.dynamodb_user]
+}
+
+resource "aws_iam_user_policy" "dynamodb_policy" {
+  name = "passgobbler-tester-dynamodb-policy"
+  user = aws_iam_user.dynamodb_user.name
+
+  policy = jsonencode({
+    "Sid": "Statement1",
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:ListTables"
+        ]
+        Resource = [aws_dynamodb_table.pass-storage.arn]
+      }
+    ]
+  })
+}
+
